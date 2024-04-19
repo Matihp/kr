75fc2b3280222a59a1c9bdf004311d95841999af
matias1364 <matiasleoncontreras@gmail.com>
2024-04-19 13:37:07 -0300

    add BacknDrop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import imagen from "@/ui/imagen.png"
 function Profile() {
   return (
+    <>
     <div className="container flex justify-center mx-auto flex-col md:flex-row">
       <div className="container sm:w-1/2  md:m-10">
         <div className="relative flex h-[40vh] w-full rounded-xl bg-cover border-2 border-red-600">
@@ -153,6 +155,34 @@ function Profile() {
         </div>
       </div>
     </div>
+    <div className="flex justify-center items-start gap-2">
+      <div>
+        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+      <Image width={100} height={96} className="object-cover w-full rounded-t-lg h-96 md:h-28 md:w-48 md:rounded-none md:rounded-s-lg" src={imagen} alt=""/>
+      <div className="flex flex-col justify-between p-4 leading-normal">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nombre del proyecto</h5>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Descripcion</p>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">link repo - link sitio web</p>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Tecnologias verificadas por Krovit: java - react ----logo otros:...........150 caracteres</p>
+          
+      </div>
+      </a>
+      </div>
+      <div>
+        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+      <Image width={100} height={96} className="object-cover w-full rounded-t-lg h-96 md:h-44 md:w-48 md:rounded-none md:rounded-s-lg" src="https://horizon-tailwind-react-git-tailwind-components-horizon-ui.vercel.app/static/media/banner.ef572d78f29b0fee0a09.png" alt=""/>
+      <div className="flex flex-col justify-between p-4 leading-normal">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+      </div>
+      </a>
+      </div>
+      
+    </div>
+    <div>
+   
+    </div>
+    </>
   );
 }
 
