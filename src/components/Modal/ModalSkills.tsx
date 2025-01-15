32de2740db706a5824b6d9643a7e26702ebbe531
Matias Contreras <matiasleoncontreras@gmail.com>
2025-01-14 21:45:30 -0300

    delete icon

--- src/components/Modal/ModalSkills.tsx
+++ src/components/Modal/ModalSkills.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState, ChangeEvent, KeyboardEvent, useEffect } from "react"
+import { useState } from "react"
 import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 import { PencilIcon } from "../ui/icons"
@@ -59,25 +59,5 @@ export default function ModalSkills({ skills, onSkillsUpdate }: ModalSkillsProps
         </DialogFooter>
       </DialogContent>
     </Dialog>
-  )
-}
-
-function XIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M18 6 6 18" />
-      <path d="m6 6 12 12" />
-    </svg>
   )
 }
