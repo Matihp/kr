2f2ca8e0127ca0e7db15d15a2d904d98739c0bf4
Matias Contreras <matiasleoncontreras@gmail.com>
2025-01-15 23:34:39 -0300

    update pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,39 +1,66 @@
 import {
-    Pagination,
-    PaginationContent,
-    PaginationEllipsis,
-    PaginationItem,
-    PaginationLink,
-    PaginationNext,
-    PaginationPrevious,
-  } from "@/components/ui/pagination"
-  
-  export function Paginations() {
-    return (
-      <Pagination className="mt-8">
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious className="mxsm:pl-1" href="#" />
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+
+interface PaginationsProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+export function Paginations({ currentPage, totalPages, onPageChange }: PaginationsProps) {
+  const showEllipsis = totalPages > 5;
+  const pages = showEllipsis 
+    ? currentPage > 3 
+      ? currentPage + 2 >= totalPages 
+        ? [1, '...', totalPages - 2, totalPages - 1, totalPages]
+        : [1, '...', currentPage - 1, currentPage, currentPage + 1, '...', totalPages]
+      : [1, 2, 3, 4, '...', totalPages]
+    : Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  return (
+    <Pagination className="mt-8">
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious 
+            className="mxsm:pl-1 cursor-pointer" 
+            onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
+            aria-disabled={currentPage === 1}
+          />
+        </PaginationItem>
+
+        {pages.map((page, index) => (
+          <PaginationItem key={index}>
+            {page === '...' ? (
+              <PaginationEllipsis />
+            ) : (
+              <PaginationLink
+                onClick={() => onPageChange(Number(page))}
+                isActive={currentPage === page}
+                className="cursor-pointer"
+              >
+                {page}
+              </PaginationLink>
+            )}
           </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">1</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#" isActive>
-              2
-            </PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">3</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext className="mxsm:pl-1" href="#" />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
-    )
-  }
+        ))}
+
+        <PaginationItem>
+          <PaginationNext 
+            className="mxsm:pl-1 cursor-pointer" 
+            onClick={() => currentPage < totalPages && onPageChange(currentPage + 1)}
+            aria-disabled={currentPage === totalPages}
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  )
+}
+
   
