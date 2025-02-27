ca51c33f91a5175a4e0e7729ac5f1bb7964d3eb1
matias1364 <matiasleoncontreras@gmail.com>
2025-02-27 20:19:24 -0300

    fix onboarding-router

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,31 @@
-'use client'
+'use client';
 
-import { ThemeProvider } from 'next-themes'
+import { ReactNode, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { ThemeProvider } from 'next-themes';
+import { useAuth } from '@/lib/useAuth';
+import { useOnboarding } from '@/lib/store/useOnboarding';
 
-export function Providers({ children }: { children: React.ReactNode }) {
-    return <ThemeProvider attribute="class" defaultTheme='system' enableSystem>{children}</ThemeProvider>
+interface ProvidersProps {
+  children: ReactNode;
 }
+
+export function Providers({ children }: ProvidersProps) {
+    const router = useRouter();
+    const { isAuthenticated, user } = useAuth();
+  
+    useEffect(() => {
+        if(user){
+             if(isAuthenticated && !user.userType) {
+                router.push('/onboarding');
+            }            
+        }
+
+    }, [isAuthenticated, user, router]);
+  
+    return (
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
+    );
+  }
