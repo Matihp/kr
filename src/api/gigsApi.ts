105bb9e3271f5c0583a2a9ac5a7f7fc9334cad67
matias1364 <matiasleoncontreras@gmail.com>
2025-03-01 21:31:15 -0300

    add gigs-api

---
+++ src/api/gigsApi.ts
@@ -0,0 +1,55 @@
+import { api } from "./api";
+
+export const fetchGigs = async () => {
+    try {
+      const response = await api.get('/gigs');
+      return response.data;
+    } catch (error) {
+      throw new Error('Error fetching gigs');
+    }
+  };
+  
+  export const fetchMyGigs = async () => {
+    try {
+      const response = await api.get('/gigs/my-gigs');
+      return response.data;
+    } catch (error) {
+      throw new Error('Error fetching your gigs');
+    }
+  };
+  
+  export const fetchGigById = async (id: string) => {
+    try {
+      const response = await api.get(`/gigs/${id}`);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error fetching gig details');
+    }
+  };
+  
+  export const updateGig = async (id: string, gigData: any) => {
+    try {
+      const response = await api.put(`/gigs/${id}`, gigData);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error updating gig');
+    }
+  };
+  
+  export const deleteGig = async (id: string) => {
+    try {
+      const response = await api.delete(`/gigs/${id}`);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error deleting gig');
+    }
+  };
+  
+  export const updateGigStage = async (gigId: string, stageId: string, data: any) => {
+    try {
+      const response = await api.patch(`/gigs/${gigId}/stages/${stageId}`, data);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error updating gig stage');
+    }
+  };
